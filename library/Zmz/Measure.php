d70fd5369f0cc9a9a503ad3c361f15e16b4369b6
Massimo Zappino <massimo.zappino@gmail.com>
2011-08-31 23:38:08 +0200

    Zmz_Measure::filterPrice

---
+++ library/Zmz/Measure.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Zmz
+ *
+ * LICENSE
+ *
+ * This source file is subject to the GNU GPLv3 license that is bundled
+ * with this package in the file COPYNG.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @copyright  Copyright (c) 2010-2011 Massimo Zappino (http://www.zappino.it)
+ * @license    http://www.gnu.org/licenses/gpl-3.0.html     GNU GPLv3 License
+ */
+class Zmz_Measure
+{
+
+    public static function filterPrice($price, $decimal = 2)
+    {
+        $price = (float) str_replace(',', '.', $price);
+        $price = number_format($price, $decimal, '.', '');
+
+        return $price;
+    }
+
+}
+
