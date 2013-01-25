93a208cf58b55870385f9e747f73da0a94dc4bbf
Massimo Zappino <massimo.zappino@gmail.com>
2013-01-25 15:09:15 +0100

    .

--- tests/library/Zend/VersionTest.php
+++ tests/library/Zend/VersionTest.php
@@ -14,6 +14,6 @@ class Zend_VersionTest extends PHPUnit_Framework_TestCase
      */
     public function testVersion()
     {
-        $this->assertFalse(version_compare(Zend_Version::VERSION, '1.11', '>='));
+        $this->assertTrue(version_compare(Zend_Version::VERSION, '1.11', '>='));
     }
 }
