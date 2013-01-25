f7e3d58783dadcf85bb0b8c745ebe72fb7b72611
Massimo Zappino <massimo.zappino@gmail.com>
2013-01-25 13:51:09 +0100

    .

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 
-define('BASE_PATH', realpath(dirname(__FILE__) . '/../'));
-define('APPLICATION_PATH', BASE_PATH . '/application');
-define('TEST_PATH', BASE_PATH . '/tests');
-define('APPLICATION_ENV', 'testing');
+defined('BASE_PATH') ? '' : define('BASE_PATH', realpath(dirname(__FILE__) . '/../'));
+defined('APPLICATION_PATH') ? '' : define('APPLICATION_PATH', BASE_PATH . '/application');
+defined('TEST_PATH') ? '' : define('TEST_PATH', BASE_PATH . '/tests');
+defined('APPLICATION_ENV') ? '' : define('APPLICATION_ENV', 'testing');
 
 
 // Ensure library/ is on include_path
@@ -18,4 +18,17 @@ date_default_timezone_set('Europe/Paris');
 require_once 'Zend/Loader/Autoloader.php';
 $loader = Zend_Loader_Autoloader::getInstance();
 
+
+// Ensure library/ is on include_path
+set_include_path(
+    implode(
+        PATH_SEPARATOR,
+        array(
+            realpath(APPLICATION_PATH . '/../library'),
+            get_include_path(),
+        )
+    )
+);
+
 require_once 'ControllerTestCase.php';
+//require_once 'TestListener.php';
--- tests/ControllerTestCase.php
+++ tests/ControllerTestCase.php
@@ -1,5 +1,6 @@
 <?php
-
+require_once 'Zend/Application.php';
+require_once 'Zend/Test/PHPUnit/ControllerTestCase.php';
 abstract class ControllerTestCase extends Zend_Test_PHPUnit_ControllerTestCase
 {
 
